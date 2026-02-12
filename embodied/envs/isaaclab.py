527187e6ca609bb15189997ea9b9556cef021724
Isaac Peterson <isaacwilliam4@gmail.com>
2026-02-12 16:12:33 -0700

    updated isaaclabenv in dreamer to support proper observation spaces now

--- embodied/envs/isaaclab.py
+++ embodied/envs/isaaclab.py
@@ -7,6 +7,7 @@ import embodied
 import numpy as np
 import gymnasium as gym
 from gym.spaces import Box
+import torch
 
 _ENV = None
 
@@ -20,12 +21,12 @@ class IsaacLabEnv(embodied.Env):
   def __init__(self, env, env_args, obs_key='image', act_key='action', **kwargs):
     self._env = gym.make(env, cfg=env_args['config'], render_mode="rgb_array", **env_args)
 
-    self._obs_dict = hasattr(self._env.observation_space, 'spaces')
-    self._act_dict = hasattr(self._env.action_space, 'spaces')
     self._obs_key = obs_key
     self._act_key = act_key
     self._done = True
     self._info = None
+
+    self._all_keys = set([self._obs_key, self._act_key, 'reward', 'is_first', 'is_last', 'is_terminal'])
     
   @property
   def env(self):
@@ -37,25 +38,20 @@ class IsaacLabEnv(embodied.Env):
 
   @functools.cached_property
   def obs_space(self):
-    if self._obs_dict:
-      spaces = self._flatten(self._env.observation_space.spaces)
-    else:
-      spaces = {self._obs_key: self._env.observation_space}
-    spaces = {k: self._convert(v) for k, v in spaces.items()}
-    return {
-        **spaces,
-        'reward': elements.Space(np.float32),
-        'is_first': elements.Space(bool),
-        'is_last': elements.Space(bool),
-        'is_terminal': elements.Space(bool),
+    num_envs = self._env.env.scene.num_envs
+    spaces = {
+      self._obs_key: self._env.observation_space,
+      'reward': Box(-np.inf, np.inf, (num_envs,), dtype=np.float32),
+      'is_first': Box(False, True, (num_envs,), dtype=bool),
+      'is_last': Box(False, True, (num_envs,), dtype=bool),
     }
+    spaces = {k: self._convert(v) for k, v in spaces.items()}
+    return spaces
 
   @functools.cached_property
   def act_space(self):
-    if self._act_dict:
-      spaces = self._flatten(self._env.action_space.spaces)
-    else:
-      spaces = {self._act_key: self._env.action_space}
+
+    spaces = {self._act_key: self._env.action_space}
     spaces = {k: self._convert(v) for k, v in spaces.items()}
     spaces['reset'] = elements.Space(bool)
     return spaces
@@ -64,19 +60,30 @@ class IsaacLabEnv(embodied.Env):
     if action['reset'] or self._done:
       self._done = False
       obs = self._env.reset()
-      return self._obs(obs, 0.0, is_first=True)
-    if self._act_dict:
-      action = self._unflatten(action)
-    else:
-      action = action[self._act_key]
-    obs, reward, self._done, self._info = self._env.step(action)
-    return self._obs(
-        obs, reward,
-        is_last=bool(self._done),
-        is_terminal=bool(self._info.get('is_terminal', self._done)))
+      _obs = obs[0]['policy']
+      return self._obs(_obs, 0.0, is_first=True)
+ 
+    action = action[self._act_key]
+    action = torch.from_numpy(action)
+    obs, reward, done, timeouts, self._info = self._env.step(action)
+    _obs = obs['policy']
+    self._done = np.logical_or(done.cpu().numpy(), timeouts.cpu().numpy())
 
+    return self._obs(
+        _obs, reward,
+        is_last=self._done,
+        is_terminal=self._done
+    )
+  
   def _obs(
       self, obs, reward, is_first=False, is_last=False, is_terminal=False):
+    
+    if isinstance(obs, torch.Tensor):
+      obs = obs.cpu()
+    
+    if isinstance(reward, torch.Tensor):
+      reward = reward.cpu().numpy()
+
     if not self._obs_dict:
       obs = {self._obs_key: obs}
     obs = self._flatten(obs)
